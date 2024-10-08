cf8207cf27b24725aea43accc10b2aa9b5a58c36
ThabisoHSN <gnyathela@gmail.com>
2024-10-08 23:22:10 +0200

    yes

--- SharedLibrary/obj/Debug/net8.0/SharedLibrary.AssemblyInfo.cs
+++ SharedLibrary/obj/Debug/net8.0/SharedLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5b993d60d5c808f10cea22b1410990ea06804e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42ca909ad17c4f1813b0e63203ead4d16107d616")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
